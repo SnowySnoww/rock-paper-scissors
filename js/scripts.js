1d35381428d130394e719cba9c35b0e23b7db41c
SnowySnoww <felipe.rrneves@gmail.com>
2022-06-28 15:14:47 -0300

    Create Rock, Paper, Scissors game
    Play rock, paper, scissors with the computer in the console.
    Created html page to use js in the browser.

---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+let computerChoice;
+let playerChoice;
+let result;
+
+for(n = 0; n <= 4; n++){
+    playRound();
+    console.log(getResult());
+}
+
+function playRound(){
+    computerPlay(0, 2);
+    playerPlay();
+    testResult();
+}
+
+function getRandomNum(min, max){
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+function computerPlay(min, max){
+    computerChoice = getRandomNum(min, max);
+    switch(computerChoice){
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break;
+    }
+    return computerChoice;
+}
+
+function playerPlay(){
+    let validChoice = false;
+    while(validChoice == false){
+        playerChoice = prompt(`Type "Rock", "Paper" or "Scissors"! (or 1, 2 or 3)`).toLowerCase().trim();
+    if (playerChoice == "rock" || playerChoice == "1"){
+        if(playerChoice == "1"){
+            playerChoice = "rock";
+        }
+        validChoice = true;
+        break;
+    }else if(playerChoice == "paper" || playerChoice == "2"){
+        if(playerChoice == "2"){
+            playerChoice = "paper";
+        }
+        validChoice = true;
+        break;
+    }else if(playerChoice == "scissors" || playerChoice == "3"){
+        if(playerChoice == "3"){
+            playerChoice = "scissors";
+        }
+        validChoice = true;
+        break;
+    }else{
+        alert("Invalid choice.")
+        validChoice = false;
+    }
+    }
+}
+
+function testResult(){
+    if(computerChoice == "rock"){
+        if(playerChoice == "rock"){
+            return result = 0;
+        }else if(playerChoice == "paper"){
+            return result = 2;
+        }else{
+            return result = 1;
+        }
+    }else if (computerChoice == "paper"){
+        if(playerChoice == "rock"){
+            return result = 1;
+        }else if(playerChoice == "paper"){
+            return result = 0;
+        }else{
+            return result = 2;
+        }
+    }else if (computerChoice == "scissors"){
+        if(playerChoice == "rock"){
+            return result = 2;
+        }else if(playerChoice == "paper"){
+            return result = 1;
+        }else{
+            return result = 0;
+        }
+    }
+}
+
+function getResult(){
+    if (result == 0){
+        return `The Player chose ${playerChoice} and the Computer chose ${computerChoice}!\nIt's a tie!`;
+    }else if(result == 1){
+        return `The Player chose ${playerChoice} and the Computer chose ${computerChoice}!\nThe Computer won!`;
+    }else if (result == 2){
+        return `The Player chose ${playerChoice} and the Computer chose ${computerChoice}!\nThe Player won!`
+    }
+}
